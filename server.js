1b29d72a7c8f469df0318048ec3850170d00878b
Veeresh Hulsogi <hulsogiveeresh@gmail.com>
2026-01-04 23:06:42 +0530

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // SERVER START
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port 5000");
-});
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
